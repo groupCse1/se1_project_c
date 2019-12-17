a8af6688ec86ba4459ca1ffcc554e3109f2226a5
aeromeror <56206150+aeromeror@users.noreply.github.com>
2019-12-16 23:40:58 -0500

    Update SubjectRepository.java

--- tutorship-back/src/main/java/co/edu/unal/tutorshipback/repository/SubjectRepository.java
+++ tutorship-back/src/main/java/co/edu/unal/tutorshipback/repository/SubjectRepository.java
@@ -1,3 +1,4 @@
+
 package co.edu.unal.tutorshipback.repository;
 
 import co.edu.unal.tutorshipback.model.Subject;
@@ -7,4 +8,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubjectRepository extends JpaRepository <Subject, Long>{
+   @Query(
+
+            value = "SELECT * FROM subject u WHERE u.subject_name = ?1",
+
+            nativeQuery = true)
+
+    User findSubjectByName(String name);
 }
