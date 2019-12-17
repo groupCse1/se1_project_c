e4e0a84a61e16f9713c44633634141f6d383b871
aeromeror <56206150+aeromeror@users.noreply.github.com>
2019-12-16 21:47:58 -0500

    fixing

--- tutorship-back/src/main/java/co/edu/unal/tutorshipback/repository/UserRepository.java
+++ tutorship-back/src/main/java/co/edu/unal/tutorshipback/repository/UserRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
   @Query("select u from user u where u.user = ?1")
-  Long getuserbyUsername(String username);
+  User getuserbyUsername(String username);
 
 }
